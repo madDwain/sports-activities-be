035cfe41702d9efc09799b29e30cc77ea6961b9f
Dwain Madolid <dwainmadolid@hotmail.co.uk>
2024-05-20 16:55:17 +0100

    added some event test data

---
+++ db/data/test-data/events.js
@@ -0,0 +1,32 @@
+module.exports = [
+  {
+    event_name: "My cool basketball scrimmage",
+    host: "DaddyDwain",
+    location: "London",
+    date: "12/12/24",
+    category: "basketball",
+    age_range: "18+",
+    price: 6,
+    capacity: 12,
+  },
+  {
+    event_name: "The wicked dodgeball match",
+    host: "dodgeball_king",
+    location: "London",
+    date: "11/11/24",
+    category: "dodgeball",
+    age_range: "16-18",
+    price: 10,
+    capacity: 18,
+  },
+  {
+    event_name: "Championship Final UEFA",
+    host: "cronaldo",
+    location: "London",
+    date: "10/10/24",
+    category: "football",
+    age_range: "21+",
+    price: 4,
+    capacity: 22,
+  },
+];
