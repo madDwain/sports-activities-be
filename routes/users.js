86657a45ba465bddd395210d70ec763f26f86513
Gurtaj <gurtajs98@gmail.com>
2024-05-23 12:04:51 +0100

    delete-user

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,7 @@
 const { getAllUsers } = require("../controllers/get-controllers");
 const { postUser } = require("../controllers/post-controllers")
 const {patchUser} = require("../controllers/patch-controllers")
+const {deleteUser} = require("../controllers/delete-controllers")
 
 const express = require("express")
 const router = express.Router()
@@ -8,5 +9,6 @@ const router = express.Router()
 router.get("", getAllUsers);
 router.post("", postUser);
 router.patch("/:username", patchUser)
+router.delete("/:username", deleteUser)
 
 module.exports = router
