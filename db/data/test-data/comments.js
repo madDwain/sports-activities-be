464602290fd989e3a3f0c85d63eee13fd7462d4d
Dwain Madolid <dwainmadolid@hotmail.co.uk>
2024-05-24 16:14:06 +0100

    added delete-comment

--- db/data/test-data/comments.js
+++ db/data/test-data/comments.js
@@ -4,69 +4,59 @@ module.exports = [
     username: "dodgeball_queen",
     event_id: 1,
     date: "10/10/24 16:00",
-    comment_id: 1
   },
   {
     body: "Itaque quisquam est similique et est perspiciatis reprehenderit voluptatem autem. Voluptatem accusantium eius error adipisci quibusdam doloribus.",
     username: "federer",
     event_id: 4,
     date: "10/11/24 12:00",
-    comment_id: 2
   },
   {
     body: "Itaque quisquam est similique et est perspiciatis reprehenderit voluptatem autem. Voluptatem accusantium eius error adipisci quibusdam doloribus.",
     username: "cronaldo",
     event_id: 2,
     date: "11/11/24 12:00",
-    comment_id: 3
   },
   {
     body: "Itaque quisquam est similique et est perspiciatis reprehenderit voluptatem autem. Voluptatem accusantium eius error adipisci quibusdam doloribus.",
     username: "DaddyDwain",
     event_id: 3,
     date: "12/02/24 11:00",
-    comment_id: 4
   },
   {
     body: "Itaque quisquam est similique et est perspiciatis reprehenderit voluptatem autem. Voluptatem accusantium eius error adipisci quibusdam doloribus.",
     username: "dodgeball_queen",
     event_id: 4,
     date: "12/12/24 18:00",
-    comment_id: 5
   },
   {
     body: "Itaque quisquam est similique et est perspiciatis reprehenderit voluptatem autem. Voluptatem accusantium eius error adipisci quibusdam doloribus.",
     username: "cronaldo",
     event_id: 2,
     date: "10/12/24 18:00",
-    comment_id: 6
   },
   {
     body: "Itaque quisquam est similique et est perspiciatis reprehenderit voluptatem autem. Voluptatem accusantium eius error adipisci quibusdam doloribus.",
     username: "DaddyDwain",
     event_id: 2,
     date: "12/12/24 18:00",
-    comment_id: 7
   },
   {
     body: "Itaque quisquam est similique et est perspiciatis reprehenderit voluptatem autem. Voluptatem accusantium eius error adipisci quibusdam doloribus.",
     username: "dodgeball_king",
     event_id: 3,
     date: "02/08/24 14:00",
-    comment_id: 8
   },
   {
     body: "Itaque quisquam est similique et est perspiciatis reprehenderit voluptatem autem. Voluptatem accusantium eius error adipisci quibusdam doloribus.",
     username: "dodgeball_king",
     event_id: 1,
     date: "12/12/24 18:00",
-    comment_id: 9
   },
   {
     body: "Itaque quisquam est similique et est perspiciatis reprehenderit voluptatem autem. Voluptatem accusantium eius error adipisci quibusdam doloribus.",
     username: "federer",
     event_id: 3,
     date: "08/12/24 15:00",
-    comment_id: 10
   },
 ];
