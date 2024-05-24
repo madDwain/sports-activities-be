9cb3b461054ee2d558d410a3b2f65c5caf74955d
Dwain Madolid <dwainmadolid@hotmail.co.uk>
2024-05-24 16:17:37 +0100

    added some dev comments

---
+++ db/data/development-data/comments.js
@@ -0,0 +1,62 @@
+module.exports = [
+  {
+    body: "Itaque quisquam est similique et est perspiciatis reprehenderit voluptatem autem. Voluptatem accusantium eius error adipisci quibusdam doloribus.",
+    username: "dodgeball_queen",
+    event_id: 1,
+    date: "10/10/24 16:00",
+  },
+  {
+    body: "Itaque quisquam est similique et est perspiciatis reprehenderit voluptatem autem. Voluptatem accusantium eius error adipisci quibusdam doloribus.",
+    username: "federer",
+    event_id: 4,
+    date: "10/11/24 12:00",
+  },
+  {
+    body: "Itaque quisquam est similique et est perspiciatis reprehenderit voluptatem autem. Voluptatem accusantium eius error adipisci quibusdam doloribus.",
+    username: "cronaldo",
+    event_id: 2,
+    date: "11/11/24 12:00",
+  },
+  {
+    body: "Shoutout my label thats me!",
+    username: "DaddyDwain",
+    event_id: 5,
+    date: "12/02/24 11:00",
+  },
+  {
+    body: "Itaque quisquam est similique et est perspiciatis reprehenderit voluptatem autem. Voluptatem accusantium eius error adipisci quibusdam doloribus.",
+    username: "dodgeball_queen",
+    event_id: 4,
+    date: "12/12/24 18:00",
+  },
+  {
+    body: "Itaque quisquam est similique et est perspiciatis reprehenderit voluptatem autem. Voluptatem accusantium eius error adipisci quibusdam doloribus.",
+    username: "cronaldo",
+    event_id: 2,
+    date: "10/12/24 18:00",
+  },
+  {
+    body: "Im in this place with TP, im in this place with 4Trey",
+    username: "DaddyDwain",
+    event_id: 6,
+    date: "12/12/24 18:00",
+  },
+  {
+    body: "Itaque quisquam est similique et est perspiciatis reprehenderit voluptatem autem. Voluptatem accusantium eius error adipisci quibusdam doloribus.",
+    username: "dodgeball_king",
+    event_id: 3,
+    date: "02/08/24 14:00",
+  },
+  {
+    body: "Itaque quisquam est similique et est perspiciatis reprehenderit voluptatem autem. Voluptatem accusantium eius error adipisci quibusdam doloribus.",
+    username: "dodgeball_king",
+    event_id: 1,
+    date: "12/12/24 18:00",
+  },
+  {
+    body: "Itaque quisquam est similique et est perspiciatis reprehenderit voluptatem autem. Voluptatem accusantium eius error adipisci quibusdam doloribus.",
+    username: "federer",
+    event_id: 3,
+    date: "08/12/24 15:00",
+  },
+];
